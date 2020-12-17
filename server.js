b4f1a1286a899eee97d0545422d2fa5ca32e1033
chefbrams <oldcitycatering@gmail.com>
2020-12-17 12:58:17 -0500

    added html route to see if it helped with heroku deployment

--- server.js
+++ server.js
@@ -28,6 +28,12 @@ mongoose.connect(
 // routes
 app.use(require("./routes/api.js"));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
